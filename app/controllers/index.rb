84a8a2f67d95bff8a9ea723709e695a0ded8fa7d
Apprentice <apprentice@dbc-chicago-024.local>
2014-02-19 10:25:39 -0600

    sdsf

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,17 +3,26 @@ get '/' do
   erb :index
 end
 
+get '/:original_url' do
+end
+
+get '/:short_url' do
+  # redirect to appropriate "long" URL
+  @url = Url.where(short_url: params[:short_url])
+  # binding.pry
+  # redirect to '@url.original_url'
+end
+
+# get '/urls'
+#   display all urls
+# end
+
 post '/urls' do
-  params[:short_url] = ("a".."z").to_a.sample(4).join("")
-  @new_url = Url.create(params[:url])
-  # redirect to '/'
-  binding.pry
+  short_url = ("a".."z").to_a.sample(4).join("")
+  @new_url = Url.create(original_url: params[:original_url], short_url: short_url)
+  # binding.pry
   erb :display_short
 end
 
 # e.g., /q6bda
-get '/:short_url' do
-  # redirect to appropriate "long" URL
-  @original_url = Url.where(short_url: params[:short_url])
-  redirect to '#{@original_url.original_url}'
-end
+
