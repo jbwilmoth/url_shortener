f8cffa00be4282963d0cf10f7887905cd613b950
Apprentice <apprentice@dbc-chicago-024.local>
2014-02-19 09:51:13 -0600

    saflkdsj

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,6 +7,7 @@ post '/urls' do
   params[:short_url] = ("a".."z").to_a.sample(4).join("")
   @new_url = Url.create(params[:url])
   # redirect to '/'
+  binding.pry
   erb :display_short
 end
 
@@ -14,5 +15,5 @@ end
 get '/:short_url' do
   # redirect to appropriate "long" URL
   @original_url = Url.where(short_url: params[:short_url])
-  erb
+  redirect to '#{@original_url.original_url}'
 end
