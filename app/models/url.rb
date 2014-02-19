ac0d7a07da710cfd167182e269ddb2894b02464e
Apprentice <apprentice@dbc-chicago-024.local>
2014-02-19 11:10:04 -0600

    testing validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,9 @@
 class Url < ActiveRecord::Base
   # Remember to create a migration!
+  validates :original_url, presence: true
+  validates_format_of :original_url, :with => /\Ahttp:\/\/.+\.\w{3}\z/i
   before_save :generate_url
+
 
   def generate_url
     self.short_url = ("a".."z").to_a.sample(4).join("")
