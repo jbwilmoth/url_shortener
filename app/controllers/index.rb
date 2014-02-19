9f61c36a346aa5e38b2aba44a0f4a4481d39c590
Apprentice <apprentice@dbc-chicago-024.local>
2014-02-19 08:30:49 -0600

    frameworked

---
+++ app/controllers/index.rb
@@ -0,0 +1,13 @@
+get '/' do
+  # let user create new short URL, display a list of shortened URLs
+  erb
+end
+
+post '/urls' do
+  # create a new Url
+end
+
+# e.g., /q6bda
+get '/:short_url' do
+  # redirect to appropriate "long" URL
+end
